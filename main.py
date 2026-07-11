56bb6719e1bac694be31c9820a5e6a81cd9ea1af
welcome shongwe <welcomeshongwe1016@gmail.com>
2026-07-11 20:52:09 +0200

    Create Database #our API has communication with a database, so we need to create a database for our API to store data

--- main.py
+++ main.py
@@ -3,7 +3,20 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+#Create Database
+#our API has communication with a database, so we need to create a database for our API to store data       
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///travel.db' #this is the location of our database, we are using sqlite for this project
+
+db = SQLAlchemy(app)
+
+
+
+#Create routes
+# a route is something like https://www.google.com/ or https://www.youtube.com/
+@app.route('/')    # a home page is defined by the '/' route
+def home():
+    return "Hello, World!" # this will be displayed on the home page
 
 
 
